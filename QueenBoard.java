9958679b8978942ed9d4084f5767b3b4262868b7
bshpetner00 <bshpetner00@stuy.edu>
2019-02-07 11:36:01 -0500

    crafted constructor and priv method

---
+++ QueenBoard.java
@@ -0,0 +1,24 @@
+public class QueenBoard {
+  private int[][]board;
+
+  public QueenBoard(int size){
+    board = new int[size][size];
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        board[i][j] = 0;
+      }
+    }
+  }
+
+  private boolean addQueen(int r, int c) {
+    if (board[r][c] == 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+
+
+}
